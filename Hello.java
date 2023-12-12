cc9bd9a2f9820d95bfc363d0c30be36b45faa8da
tut211839 <153584625+tut211839@users.noreply.github.com>
2023-12-12 15:42:20 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-Class Hello {
-  static void main() {
-    System.out.print("hello world.")
+public class Hello {
+  public static void main(String[] args) {
+    System.out.print("hello world.");
   }
 }
